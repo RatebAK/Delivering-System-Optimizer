5362d94cc3f5cd39d3227e21ab661bf40892bd30
RatebAK <74676374+RatebAK@users.noreply.github.com>
2024-05-14 00:52:23 +0300

    Update Truck.py

--- Entities/Truck.py
+++ Entities/Truck.py
@@ -6,3 +6,11 @@ class Truck:
     def __init__(self, name, capacity):
         self.name = name
         self.capacity = capacity
+
+    def addLoad(self, *arg):
+        for load in arg:
+            self.loads.append(load)
+
+    def addPathPoint(self, *arg):
+        for point in arg:
+            self.loads.append(point)
